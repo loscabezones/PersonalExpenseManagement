fb1e0326471e53b686c6e05229750e3498149579
David Espin <david.espin.morales@everis.com>
2019-09-09 18:31:25 +0200

    stroture

--- PersonalExpenseManagement/src/app/app.module.ts
+++ PersonalExpenseManagement/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
